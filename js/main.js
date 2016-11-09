b1089d661226610e47f1fe78947f08354dffcde0
Gustavo Murcia <gmurcia@uwo.ca>
2016-11-09 13:18:46 -0500

    Added Hover.css for the contact icons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $("#name h1").addClass("load");
 $("#description p").addClass("load");
 $(".col-md-3 div").addClass("load");
+
+
+// To make the animation quit after clicking use this:
+// .finish()
+//              http://api.jquery.com/finish/
+// and the click function above
+
+/* Color animation for jokes
+$('body').addClass('logo-loading');
+*/
