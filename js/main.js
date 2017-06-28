bd47fb1fd25ab99e927b569e1a5f9ebbcad2e0cb
Gustavo Murcia <gmurcia@uwo.ca>
2017-06-27 20:32:20 -0400

    Added better styling to avoid scrolling on large screens - and faster timing on animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $("#name h1").addClass("load");
+$("#profile-pic img").addClass("load");
 $("#description p").addClass("load");
-$(".col-md-3 div").addClass("load");
+$(".col-xs-3 div").addClass("load");
 
 
 // To make the animation quit after clicking use this:
@@ -8,6 +9,5 @@ $(".col-md-3 div").addClass("load");
 //              http://api.jquery.com/finish/
 // and the click function above
 
-/* Color animation for jokes
+// Color animation for jokes
 $('body').addClass('logo-loading');
-*/
